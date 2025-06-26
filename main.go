5e0880c7024d94e62b00d1ec54a6c51fbfd7c59c
edgardcham <edgardelcham@gmail.com>
2025-06-26 20:23:18 +0400

    Added DB stuff

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 	"sync/atomic"
+
+	"os"
+
+	"github.com/edgardcham/go-http-server/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	// get db url
+	dbURL := os.Getenv("DB_URL")
+	// open connection
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+
+	if err != nil {
+		fmt.Println("Couldn't establish connection to DB")
+		os.Exit(1)
+	}
 	// This defines a multiplexer/router
 	mux := http.NewServeMux()
 	apiConfig := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
 	}
 
 	// this instantiates the server
